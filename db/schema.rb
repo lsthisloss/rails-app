90280c99951f6fb0251e024a72d7c0a2aaeb0e17
oleh <your-email>
2023-03-13 11:23:29 +0200

    last

--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_13_052114) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "role"
+    t.integer "rolem", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
