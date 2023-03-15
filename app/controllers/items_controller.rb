0780b13fc2dc32b1a206fff0aff8a5e5626b94e0
oleh <your-email>
2023-03-15 17:31:51 +0200

    git cache cleared

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: %i[ show edit update destroy ]
 
+  # POST /items or /items.json
+  
   # GET /items or /items.json
   def index
     @items = Item.all
@@ -19,20 +21,6 @@ class ItemsController < ApplicationController
   def edit
   end
 
-  # POST /items or /items.json
-  def create
-    @item = Item.new(item_params)
-
-    respond_to do |format|
-      if @item.save
-        format.html { redirect_to item_url(@item), notice: "Item was successfully created." }
-        format.json { render :show, status: :created, location: @item }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
   # PATCH/PUT /items/1 or /items/1.json
   def update
@@ -67,4 +55,28 @@ class ItemsController < ApplicationController
     def item_params
       params.require(:item).permit(:name, :description, :price)
     end
+
+    private
+
+  # This should probably be abstracted to ApplicationController
+  def authorize_admin
+    redirect_to "/",  status: 401 unless (current_user.try(:admin?))
+  end
+
+  before_action :authorize_admin, :except => [:index, :show]
+
+  def create
+    @item = Item.new(item_params)
+
+    respond_to do |format|
+      if @item.save
+        format.html { redirect_to item_url(@item), notice: "Item was successfully created." }
+        format.json { render :show, status: :created, location: @item }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 end
