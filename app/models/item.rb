9fa4500d13177e479a8e2f2836f8500fabdc53a9
oleh <your-email>
2023-04-13 17:08:15 +0300

    commit for deploy to heroku

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
     has_many :myorderables
-    has_many :carts, through: :myorderables
+    has_many :carts
+
+    before_destroy(:make_sure_no_myorderables)
+
+    def make_sure_no_myorderables
+        if self.myorderables.empty?
+            return true
+        else
+            return false
+        end
+    end
 end
